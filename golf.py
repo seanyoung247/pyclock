a561cccacdb9676729e0dcf4a80e83de36a95e01
seanyoung247 <seanyoung247@gmail.com>
2022-06-01 10:55:05 +0100

    feat: Wraps code golf in function Prevents code golf version from running on import

--- golf.py
+++ golf.py
@@ -1,2 +1,7 @@
-import time as t;n='\n'
-while 1:print(n*99+n.join([' '.join([['█'*4,'█  █','   █','█   '][0x82081335720200242ded4800108>>s*23>>int(c)*2&3]for c in t.strftime("%H%M%S")])for s in range(5)])+n),t.sleep(1)
+def main():
+    import time as t;n='\n'
+    while 1:print(n*99+n.join([' '.join([['█'*4,'█  █','   █','█   '][0x82081335720200242ded4800108>>s*23>>int(c)*2&3]for c in t.strftime("%H%M%S")])for s in range(5)])+n),t.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
